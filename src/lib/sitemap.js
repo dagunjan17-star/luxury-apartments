5022ec436d4d074c99bdca6fd50647c86ac5e2fc
Mukesh Kumar <mk8701952@gmail.com>
2026-06-19 11:05:36 +0530

    code updatee

---
+++ src/lib/sitemap.js
@@ -0,0 +1,78 @@
+import axios from "axios";
+
+
+const currentDate =
+    new Date().toISOString();
+
+export async function generateSitemap() {
+  const baseUrl = "https://www.luxuryapartmentsingurgaon.com";
+
+  // 🔹 Static URLs
+  const staticUrls = `
+    <url>
+    <loc>${baseUrl}/</loc>
+     <lastmod>${currentDate}</lastmod>
+    </url>
+    <url>
+    <loc>${baseUrl}/about</loc>
+    <lastmod>${currentDate}</lastmod>
+    </url>
+    <url>
+    <loc>${baseUrl}/contact</loc>
+    <lastmod>${currentDate}</lastmod>
+    </url>
+    <url>
+  <loc>${baseUrl}/blog</loc>
+  <lastmod>${currentDate}</lastmod>
+</url>
+
+  `;
+  // 🔥 BLOG URLs
+  let blogUrls = [];
+
+  try {
+
+    const apiDomain = baseUrl
+      .replace("https://", "");
+
+    const res = await axios.get(
+      `https://gurgaon-backend.onrender.com/blogs/getSlugsByDomain/${apiDomain}`
+    );
+
+    if (
+      res.data?.success &&
+      res.data?.data?.length
+    ) {
+
+      blogUrls = res.data.data.map(
+        (slug) => `
+          <url>
+            <loc>${baseUrl}/blog/${slug}</loc>
+             <lastmod>${currentDate}</lastmod>
+          </url>
+        `
+      );
+
+    }
+
+  } catch (err) {
+
+    console.error(
+      "Blog fetch error:",
+      err
+    );
+
+  }
+
+  // 🔹 Combine all
+  const allUrls = [
+    staticUrls,
+     ...blogUrls,
+  ].join("\n");
+
+  // 🔹 XML Output
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allUrls}
+</urlset>`;
+}
